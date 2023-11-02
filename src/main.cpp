5a3644c5f6d79321b7b2058a3a32d58a53ce6bc8
jacksoncy <wuxiangwei@yalla.com>
2023-11-02 10:46:56 +0800

    整理文件

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,34 @@
 #include <cstdlib>
 #include <format>
 #include <iostream>
+#include <memory_resource>
 
-auto main() -> int {
-    std::format("sff{}", 233);
+// https://stackoverflow.com/questions/1598985/c-read-binary-stdin
+#ifdef _WIN32
+#include <fcntl.h>
+#include <io.h>
+#define SET_BINARY_MODE()                \
+    _setmode(_fileno(stdin), _O_BINARY); \
+    _setmode(_fileno(stdout), _O_BINARY)
+#else
+#define SET_BINARY_MODE() (void)0
+#endif
+
+auto main(int argc, char **argv) -> int {
+    std::cout << std::format("sff {}", 233) << std::endl;
+    if (argc > 1) {
+    } else {
+        SET_BINARY_MODE();
+
+        while (true) {
+            std::cin.peek();
+            char *buffer;
+            std::cin.readsome(buffer, 128);
+
+            std::cout.write("sfsfs\n", 7);
+
+            std::cout.flush();
+        }
+    }
     return EXIT_SUCCESS;
 }
---
+++ test/jsonrpc.hpp
@@ -0,0 +1,5 @@
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Factorials are computed", "[factorial]") {
+    REQUIRE(1 == 1);
+}
