da861f707aecb7257c9dc47dc2e862459a455892
jacksoncy <wuxiangwei@yalla.com>
2024-06-25 17:41:47 +0800

    初步的处理函数

--- include/app/app.hpp
+++ include/app/app.hpp
@@ -2,11 +2,34 @@
 
 // 应用模块
 
+#include <nlohmann/json.hpp>
+#include <string>
+
 namespace app {
 
 class App {
 public:
     App()  = default;
     ~App() = default;
+
+    static auto Initialize(nlohmann::json params) -> const std::string {
+        return "init";
+    }
+
+    static auto Initialized(nlohmann::json params) -> const std::string {
+        return "inited";
+    }
+
+    static auto DidOpen(nlohmann::json params) -> const std::string {
+        return "open";
+    }
+
+    static auto DidChange(nlohmann::json params) -> const std::string {
+        return "change";
+    }
+
+    static auto DidClose(nlohmann::json params) -> const std::string {
+        return "close";
+    }
 };
 }  // namespace app
--- include/server/server.hpp
+++ include/server/server.hpp
@@ -2,8 +2,10 @@
 
 // 用户界面层
 
+#include "app/app.hpp"
 #include "domain/entity/session.hpp"
 
+#include <functional>
 #include <memory>
 #include <nlohmann/json.hpp>
 #include <unordered_map>
@@ -16,7 +18,11 @@ public:
 
     ~Server() = default;
 
-    void Dispatch(std::string_view method, nlohmann::json params) {}
+    void Dispatch(std::string_view method, nlohmann::json params) {
+        if (auto handler = handler_.find(method.data()); handler != handler_.end()) {
+            session_->Write(handler->second(params));
+        }
+    }
 
     void Run() {
         while (true) {
@@ -29,8 +35,18 @@ public:
     }
 
 private:
-    std::shared_ptr<domain::entity::Session>                                           session_;
-    std::unordered_map<std::string, std::function<std::string(nlohmann::json params)>> handler_{};  // 接口处理
+    // 注册处理接口
+    void enroll() {
+        handler_.emplace("initialize", app_.Initialize);
+        handler_.emplace("initialized", app_.Initialized);
+        handler_.emplace("textDocument/didOpen", app_.DidOpen);
+        handler_.emplace("textDocument/didChange", app_.DidChange);
+        handler_.emplace("textDocument/didClose", app_.DidClose);
+    }
+
+    std::shared_ptr<domain::entity::Session> session_;
+    app::App                                 app_{};
+    std::unordered_map<std::string, std::function<const std::string(nlohmann::json params)>> handler_{};  // 接口处理
 };
 
 }  // namespace server
